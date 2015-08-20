56ed415101df388b4c7595b9a0644cdb95d09951
Colin Drake <colin.f.drake@gmail.com>
2015-08-20 14:21:37 -0400

    Skeleton of CLI workflow.

--- lib/robjc/cli.rb
+++ lib/robjc/cli.rb
@@ -10,13 +10,24 @@ module RObjc
     end
 
     def parse_args(args)
+      {}
     end
 
     def execute
+      load_project
+      generate_code
+    end
+
+    def load_project
+      build_output.info "Parsing project resources..."
+    end
+
+    def generate_code
+      build_output.info "Generating code..."
     end
 
     def build_output
-      @build_output ||= BuildOutput.new
+      @build_output ||= BuildOutput.new(@config)
     end
   end
 end
