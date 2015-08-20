a0ce13f1e0b606a88da14ae7ccb391cb1063bc00
Colin Drake <colin.f.drake@gmail.com>
2015-08-20 13:57:28 -0400

    Add in structure for template class.

---
+++ lib/robjc/templaterenderer.rb
@@ -0,0 +1,21 @@
+module RObjc
+  # Manages finding and rendering code templates to strings.
+  class TemplateRenderer
+    def initialize(config)
+      @config = config
+    end
+
+    private
+
+    def find_template(name)
+    end
+
+    public
+
+    def render_implementation
+    end
+
+    def render_interface
+    end
+  end
+end
