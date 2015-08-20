f6bd5555d2c0f4803dabf797ac51c7f867a02985
Colin Drake <colin.f.drake@gmail.com>
2015-08-20 14:41:39 -0400

    Additional includes.

--- lib/robjc.rb
+++ lib/robjc.rb
@@ -1,3 +1,6 @@
+# Standard library dependencies.
+require 'optparse'
+
 # Third party dependencies.
 require 'mustache'
 require 'nokogiri'
@@ -5,9 +8,12 @@ require 'rubygems'
 require 'xcodeproj'
 
 # Internal modules, etc.
+require 'robjc/buildoutput'
 require 'robjc/cli'
+require 'robjc/configuration'
 require 'robjc/codegenerator'
 require 'robjc/project'
+require 'robjc/renderable'
 require 'robjc/templaterenderer'
 require 'robjc/version'
 
