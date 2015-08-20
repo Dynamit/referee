1c12efbe9ad21ca294fc66560cb414f41c8e46e7
Colin Drake <colin.f.drake@gmail.com>
2015-08-20 13:57:53 -0400

    Add in all includes to main module file.

--- lib/robjc.rb
+++ lib/robjc.rb
@@ -1,5 +1,16 @@
-require "robjc/version"
+# Third party dependencies.
+require 'mustache'
+require 'nokogiri'
+require 'rubygems'
+require 'xcodeproj'
 
-module Robjc
-  # Your code goes here...
+# Internal modules, etc.
+require 'robjc/cli'
+require 'robjc/codegenerator'
+require 'robjc/project'
+require 'robjc/templaterenderer'
+require 'robjc/version'
+
+# Main module declaration.
+module RObjc
 end
